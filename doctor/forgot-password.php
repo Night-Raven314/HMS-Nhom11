7e636a62d69a17ae7545a5e136361b1306a7c584
haygoitoilalong <nguyenbalong1502@gmail.com>
2024-10-12 08:29:12 +0700

    add forgot pass, fix link folder

--- doctor/forgot-password.php
+++ doctor/forgot-password.php
@@ -30,12 +30,12 @@ echo "<script>window.location.href ='forgot-password.php'</script>";
 		<title>Password Recovery</title>
 		
 		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
-		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
-		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
-		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
-		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../assets/vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="../assets/vendor/themify-icons/themify-icons.min.css">
+		<link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="../assets/vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="../assets/vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
 		<link rel="stylesheet" href="assets/css/styles.css">
 		<link rel="stylesheet" href="assets/css/plugins.css">
 		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
@@ -94,13 +94,13 @@ echo "<script>window.location.href ='forgot-password.php'</script>";
 
 			</div>
 		</div>
-		<script src="vendor/jquery/jquery.min.js"></script>
-		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-		<script src="vendor/modernizr/modernizr.js"></script>
-		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
-		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-		<script src="vendor/switchery/switchery.min.js"></script>
-		<script src="vendor/jquery-validation/jquery.validate.min.js"></script>
+		<script src="../assets/vendor/jquery/jquery.min.js"></script>
+		<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="../assets/vendor/modernizr/modernizr.js"></script>
+		<script src="../assets/vendor/jquery-cookie/jquery.cookie.js"></script>
+		<script src="../assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="../assets/vendor/switchery/switchery.min.js"></script>
+		<script src="../assets/vendor/jquery-validation/jquery.validate.min.js"></script>
 	
 		<script src="assets/js/main.js"></script>
 
--- doctor/index.php
+++ doctor/index.php
@@ -40,12 +40,12 @@ header("location:index.php");
 		<title>Doctor Login</title>
 		
 		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
-		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
-		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
-		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
-		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../assets/vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="../assets/vendor/themify-icons/themify-icons.min.css">
+		<link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="../assets/vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="../assets/vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
 		<link rel="stylesheet" href="assets/css/styles.css">
 		<link rel="stylesheet" href="assets/css/plugins.css">
 		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
@@ -100,13 +100,13 @@ header("location:index.php");
 
 			</div>
 		</div>
-		<script src="vendor/jquery/jquery.min.js"></script>
-		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-		<script src="vendor/modernizr/modernizr.js"></script>
-		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
-		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-		<script src="vendor/switchery/switchery.min.js"></script>
-		<script src="vendor/jquery-validation/jquery.validate.min.js"></script>
+		<script src="../assets/vendor/jquery/jquery.min.js"></script>
+		<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="../assets/vendor/modernizr/modernizr.js"></script>
+		<script src="../assets/vendor/jquery-cookie/jquery.cookie.js"></script>
+		<script src="../assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="../assets/vendor/switchery/switchery.min.js"></script>
+		<script src="../assets/vendor/jquery-validation/jquery.validate.min.js"></script>
 	
 		<script src="assets/js/main.js"></script>
 
