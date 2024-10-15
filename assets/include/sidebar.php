63c59860c51f9fce439d07d29ccac333189351c7
haygoitoilalong <nguyenbalong1502@gmail.com>
2024-10-15 23:15:41 +0700

    header,footer,sidebar,styles

--- assets/include/sidebar.php
+++ assets/include/sidebar.php
@@ -1,16 +1,26 @@
-<section id="slider">
-        <div class="container-xl">
-            <div class="row">
-                <div class="col-lg-6 py-5 slid-detail align-self-center">
-                    <h1 class="fw-bolder fs-11">Website quản lý bệnh viện <span class="text-primary">HKL</span></h1>
-                    <p>Noi dung</p>
-                    <div class="pt-5">
-                        <button class="btn btn-primary shadow-md fw-bold p-3 px-5">Đăng kí lịch khám</button>
-                    </div>
-                </div>
-                <div class="col-lg-6 py-5 px-5">
-                    <img src="assets/image/Areas&Rooms/Hospital waiting room HKL.png" alt="">
-                </div>
-            </div>
-        </div>
-    </section>
+<aside class="sidebar">
+    <h2>#</h2>
+    <ul>
+        <li><a href="#">#</a></li>
+        <li><a href="#">#</a></li>
+        <li><a href="#">#</a></li>
+        <li><a href="#">#</a></li>
+        <li><a href="#">#</a></li>
+    </ul>
+    
+    <h2>Find Us</h2>
+    <p>
+        <i class="fas fa-map-marker-alt"></i>
+        #
+    </p>
+    
+    <h2>#</h2>
+    <p>
+        <i class="fas fa-phone-alt"></i> 
+        <a href="tel:+1234567890">#</a>
+    </p>
+    <p>
+        <i class="fas fa-envelope"></i> 
+        <a href="mailto:info@healthcarehospital.com">#</a>
+    </p>
+</aside>
