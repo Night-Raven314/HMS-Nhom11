7e8fad0284d8281be1b56bb5552fdf0e3996892a
haygoitoilalong <nguyenbalong1502@gmail.com>
2024-10-17 22:44:00 +0700

    thêm ls hẹn khám, ls khám, chi tiết khám

---
+++ manage-medhistory.php
@@ -0,0 +1,418 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+
+    <title>
+        Người dùng
+    </title>
+
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+
+    <!-- Nucleo Icons -->
+    <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+
+    <!-- CSS Files -->
+    <link id="pagestyle" href="assets/css/material_dash.css" rel="stylesheet" />
+
+</head>
+
+<!-- Side Nav -->
+
+<body class="g-sidenav-show"
+    style="background-image: url('./assets/image/Hospital_Seamless1.png'); background-size: 400px 400px;">
+
+    <aside
+        class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
+        id="sidenav-main">
+
+        <div class="sidenav-header">
+            <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+                aria-hidden="true" id="iconSidenav"></i>
+            <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
+                target="_blank">
+                <img src="assets/image/logo01-sq.png" class="navbar-brand-img h-100" alt="main_logo">
+                <span class="ms-1 font-weight-bold text-white">HKL Hospital</span>
+            </a>
+        </div>
+
+        <hr class="horizontal light mt-0 mb-2">
+
+        <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+            <ul class="navbar-nav">
+
+                <li class="nav-item mt-3">
+                    <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">ĐIỀU HƯỚNG CHÍNH
+                    </h6>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link text-white active bg-gradient-primary" href="default-Appointmenthistory.php">
+
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-icons opacity-10">table_view</i>
+                            <!-- Check https://fonts.google.com/icons?icon.set=Material+Icons&icon.style=Rounded for ID -->
+                        </div>
+
+                        <span class="nav-link-text ms-1">LỊCH SỬ HẸN KHÁM</span>
+                    </a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="./pages/dashboard.html">
+
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-icons opacity-10">lock</i>
+                            <!-- Check https://fonts.google.com/icons?icon.set=Material+Icons&icon.style=Rounded for ID -->
+                        </div>
+
+                        <span class="nav-link-text ms-1">Function 2</span>
+                    </a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="./pages/dashboard.html">
+
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-icons opacity-10">favorite</i>
+                            <!-- Check https://fonts.google.com/icons?icon.set=Material+Icons&icon.style=Rounded for ID -->
+                        </div>
+
+                        <span class="nav-link-text ms-1">Function 3</span>
+                    </a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="./pages/dashboard.html">
+
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-icons opacity-10">description</i>
+                            <!-- Check https://fonts.google.com/icons?icon.set=Material+Icons&icon.style=Rounded for ID -->
+                        </div>
+
+                        <span class="nav-link-text ms-1">Function 4</span>
+                    </a>
+                </li>
+
+
+                <li class="nav-item mt-3">
+                    <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Divider 2
+                    </h6>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="./pages/dashboard.html">
+
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-icons opacity-10">schedule</i>
+                            <!-- Check https://fonts.google.com/icons?icon.set=Material+Icons&icon.style=Rounded for ID -->
+                        </div>
+
+                        <span class="nav-link-text ms-1">Function 1</span>
+                    </a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="./pages/dashboard.html">
+
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-icons opacity-10">event</i>
+                            <!-- Check https://fonts.google.com/icons?icon.set=Material+Icons&icon.style=Rounded for ID -->
+                        </div>
+
+                        <span class="nav-link-text ms-1">Function 2</span>
+                    </a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="./pages/dashboard.html">
+
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-icons opacity-10">login</i>
+                            <!-- Check https://fonts.google.com/icons?icon.set=Material+Icons&icon.style=Rounded for ID -->
+                        </div>
+
+                        <span class="nav-link-text ms-1">Function 3</span>
+                    </a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="./pages/dashboard.html">
+
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-icons opacity-10">paid</i>
+                            <!-- Check https://fonts.google.com/icons?icon.set=Material+Icons&icon.style=Rounded for ID -->
+                        </div>
+
+                        <span class="nav-link-text ms-1">Function 4</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
+    </aside>
+    <!-- End Side Nav -->
+
+    <main class="main-content border-radius-lg ">
+        <!-- Navbar -->
+
+        <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 border-radius-xl shadow-none position-sticky blur shadow-blur mt-4 left-auto top-1 z-index-sticky"
+            id="navbarBlur" data-scroll="true">
+            <div class="container-fluid py-1 px-3">
+                <nav aria-label="breadcrumb">
+
+                    <h6 class="font-weight-bolder mb-0">LỊCH SỬ KHÁM BỆNH</h6>
+
+                </nav>
+                <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+
+                    </div>
+                    <ul class="navbar-nav  justify-content-end">
+
+                        <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                            <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                                <div class="sidenav-toggler-inner">
+                                    <i class="sidenav-toggler-line"></i>
+                                    <i class="sidenav-toggler-line"></i>
+                                    <i class="sidenav-toggler-line"></i>
+                                </div>
+                            </a>
+                        </li>
+
+                        <li class="nav-item d-flex align-items-center">
+                    </ul>
+
+                    <ul class="navbar-nav  justify-content-end">
+
+                        <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                            <!-- Right corner user section -->
+                        </li>
+                        <li class="nav-item d-flex align-items-center">
+                            <a class="btn btn-outline-primary btn-sm mb-0 me-3" target="_blank" href="sign-in.php">Đăng
+                                xuất</a>
+                        </li>
+                        
+                        </li>
+
+                        <li class="nav-item d-flex align-items-center">
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
+        <!-- End Navbar -->
+    <div class="container-fluid py-4">
+      <div class="row">
+        <div class="col-12">
+          <div class="card my-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+              <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                <h6 class="text-white text-capitalize ps-3"> THÔNG TIN BỆNH NHÂN</h6>
+              </div>
+            </div>
+            <div class="card-body px-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    
+                    
+                  </thead>
+                  <table  class="table table-bordered ">
+                  <tbody >
+                    
+                    
+                    <tr>
+                        <th scope=""style="border-top: 2px solid #003D59; border-bottom: 2px solid #003D59;">Họ và tên : </th>
+                        <td style="border:2px solid #003D59;">Nguyễn Văn A</td>
+                        <th scope=""style="border:2px solid #003D59;">Địa chỉ Email : </th>
+                        <td style="border-top: 2px solid #003D59; border-bottom: 2px solid #003D59;">anjan@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <th scope=""style="border-top: 2px solid #003D59; border-bottom: 2px solid #003D59;">Số điện thoại : </th>
+                        <td style="border:2px solid #003D59;">0925009336</td>
+                        <th scope="" style="border:2px solid #003D59;">Địa chỉ : </th>
+                        <td style="border-top: 2px solid #003D59; border-bottom: 2px solid #003D59;">TP.Hồ Chí Minh</td>
+                    </tr>
+                    <tr>
+                        <th scope=""style="border-top: 2px solid #003D59; border-bottom: 2px solid #003D59;">Giới tính : </th>
+                        <td style="border:2px solid #003D59;">Nam</td>
+                        <th scope="" style="border:2px solid #003D59;">Tuổi : </th>
+                        <td style="border-top: 2px solid #003D59; border-bottom: 2px solid #003D59;">20</td>
+                    </tr>
+                    <tr>
+                        <th scope=""style="border-top: 2px solid #003D59; border-bottom: 2px solid #003D59;">Tiền sử bệnh nhân (nếu có) : </th>
+                        <td style="border:2px solid #003D59;">Bình thường</td>
+                        <th scope="" style="border:2px solid #003D59;">Ngày/Giờ hẹn khám : </th>
+                        <td style="border-top: 2px solid #003D59; border-bottom: 2px solid #003D59;">20/10/2024 16:30</td>
+                    </tr>
+                </tbody>
+
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="card my-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+              <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                <h6 class="text-white text-capitalize ps-3"> LỊCH SỬ  KHÁM BỆNH</h6>
+              </div>
+            </div>
+            <div class="card-body px-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center justify-content-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="center">STT</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Huyết áp</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Trọng lượng</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Lượng đường trong máu</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Nhiệt độ cơ thể</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Đơn thuốc</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Ngày truy cập</th>
+                      
+                      
+                      <th class="text-secondary opacity-7"></th>
+                    </tr>
+                  </thead>
+                  
+                  <tbody >
+                    
+                    
+                    
+                    <tr>
+                        <td class="align-middle text-center">
+                            <span class="text-secondary text-xs font-weight-bold">01</span>
+                        </td>
+                        <td>
+                            <p class="text-sm font-weight-bold mb-0">20</p>
+                        </td>
+                        <td>
+                            <p class="text-sm font-weight-bold mb-0">40</p>
+                        </td>
+                        <td>
+                            <p class="text-sm font-weight-bold mb-0">30</p>
+                        </td>
+                        <td>
+                            <p class="text-sm font-weight-bold mb-0">60</p>
+                        </td>
+                        <td>
+                            <span class="text-xs font-weight-bold">Bình thường</span>
+                        </td>
+
+                        <td>
+                            <span class="text-xs font-weight-bold">20/10/2024 16:30</span>
+                        </td>
+                        
+                        
+                    </tr>
+                    
+                    
+                </tbody>
+
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+       
+<footer class="footer py-4  ">
+    <div class="container-fluid">
+      <div class="row align-items-center justify-content-lg-between">
+        <div class="col-lg-6 mb-lg-0 mb-4">
+          <div class="copyright text-center text-sm text-muted text-lg-start">
+            ©
+            <script>
+              document.write(new Date().getFullYear())
+            </script>,
+            made with <i class="fa fa-heart"></i> by
+            <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Huan, Khoa and Long</a>
+            for Uni 24-25.
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About
+                Us</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted"
+                target="_blank">License</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+    </div>
+    
+    </main>
+
+
+
+
+
+
+    <!--   Core JS Files   -->
+    <script src="./assets/js/core/popper.min.js"></script>
+    <script src="./assets/js/core/bootstrap.min.js"></script>
+    <script src="./assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="./assets/js/plugins/smooth-scrollbar.min.js"></script>
+
+
+
+    <script>
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        }
+    </script>
+
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+
+    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="assets/js/material-dashboard.min.js"></script>
+</body>
+
+</html>
