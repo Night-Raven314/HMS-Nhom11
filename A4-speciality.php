36df0115b8f265ef7e2036b9180598df6787667e
Night-Raven314 <huan.nng25@gmail.com>
2024-10-19 13:48:03 +0700

    Minor fixes

--- A4-speciality.php
+++ A4-speciality.php
@@ -461,9 +461,9 @@
         <div id="container-popup">
             <div id="popupContact">
                 <div class="card">
-                    <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                         <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                            <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Cập nhật thông tin chuyên khoa</h4>
+                            <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Cập nhật chuyên khoa</h4>
                         </div>
                     </div>
                     <div class="card-body">
@@ -472,7 +472,7 @@
                             <div class="input-group input-group-outline mb-3">
                                 <label class="form-label">Tên Chuyên Khoa</label>
                                 <input type="text" class="form-control" placeholder="">
-                            </div
+                            </div>
                                 <!-- Tên Chuyên Khoa -->
                             <div class="input-group input-group-outline mb-3">
                                 <label class="form-label">Mô Tả</label>
