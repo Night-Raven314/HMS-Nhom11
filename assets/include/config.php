d154e670a95851b73605b4c45d390935940065a8
Night-Raven314 <huan.nng25@gmail.com>
2024-10-26 20:25:39 +0700

    Bổ sung thư viện google oauth

--- assets/include/config.php
+++ assets/include/config.php
@@ -1,11 +1,20 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "dacn-hms";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "dacn-hms";
+
+define('google_app_id','104458844677-uvj7eo80ufvo6cimqoa3jr4s2rldoje2.apps.googleusercontent.com');
+define('google_app_secret','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');
+define('google_app_callback_url','http://localhost/LoginGoogle/redirect-google.php');
+define('servername','localhost');
+define('username','root');
+define('password','');
+define('dbname','dacn-hms');
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli(servername, username, password, dbname);
 
 // Check connection
 if ($conn->connect_error) {
