0cce96cb4c3feff7fe741c4cc3c31c7c24c8d135
TNDangKhoa1606 <tdangkhoa1606@gmail.com>
2024-10-30 17:05:51 +0700

    thêm popup đăng kí lịch khám, nút chỉnh sửa giờ khám

---
+++ assets/js/popup-copy.js
@@ -0,0 +1,16 @@
+// CAI NAY CUA KHOA 
+
+// Function To Display Popup
+function div_show(popupId) {
+  // Ẩn tất cả popup
+  document.getElementById("edit-popup").style.display = "none";
+  document.getElementById("register-popup").style.display = "none";
+
+  // Hiển thị popup được chỉ định
+  document.getElementById(popupId).style.display = "block";
+}
+
+// Function to Hide Popup
+function div_hide(popupId) {
+  document.getElementById(popupId).style.display = "none";
+}
