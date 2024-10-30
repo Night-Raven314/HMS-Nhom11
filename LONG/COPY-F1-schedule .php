996fab92ae84d54b06f93a1d6cae902b3413f522
TNDangKhoa1606 <tdangkhoa1606@gmail.com>
2024-10-31 01:09:11 +0700

    commit

--- LONG/COPY-F1-schedule .php
+++ LONG/COPY-F1-schedule .php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    // error_reporting(0);
-    include('L-connect.php');
+    error_reporting(0);
+    include('../L-connect.php');
     
 
 ?>
@@ -14,8 +14,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
 
     <title>
         Lịch hẹn kiểm tra
@@ -27,8 +27,8 @@
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
 
     <!-- Nucleo Icons -->
-    <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
 
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
@@ -37,7 +37,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
 
     <!-- CSS Files -->
-    <link id="pagestyle" href="assets/css/material_dash.css" rel="stylesheet" />
+    <link id="pagestyle" href="../assets/css/material_dash.css" rel="stylesheet" />
 
 </head>
 
@@ -378,10 +378,10 @@
 
 
     <!--   Core JS Files   -->
-    <script src="./assets/js/core/popper.min.js"></script>
-    <script src="./assets/js/core/bootstrap.min.js"></script>
-    <script src="./assets/js/plugins/perfect-scrollbar.min.js"></script>
-    <script src="./assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <script src="../assets/js/core/popper.min.js"></script>
+    <script src="../assets/js/core/bootstrap.min.js"></script>
+    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
 
 
 
@@ -400,7 +400,7 @@
 
 
     <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="assets/js/material-dashboard.min.js"></script>
+    <script src="../assets/js/material-dashboard.min.js"></script>
 </body>
 
 </html>
