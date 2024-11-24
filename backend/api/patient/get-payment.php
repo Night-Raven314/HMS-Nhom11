bc1ef02a1f16bc74deaeeb19c240cb4e4d14fb39
Night-Raven314 <huan.nng25@gmail.com>
2024-11-24 18:42:26 +0700

    get patient payment log

---
+++ backend/api/patient/get-payment.php
@@ -0,0 +1,46 @@
+<?php
+  // error_reporting(E_ALL);
+  // ini_set('display_errors', 1);
+  include ('../config.php');
+  
+  // Allow CORS requests (optional, based on your setup)
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json");
+  header("Access-Control-Allow-Methods: POST");
+  
+  $errorMsg = "";
+// Get the raw POST data and decode it
+$input = file_get_contents("php://input");
+// Get data sent from FE
+$data = json_decode($input, true);
+if ($data) {
+  $auth_user_id = $data['auth_user_id'] ? mysqli_real_escape_string($conn, $data['auth_user_id']) : null;
+  // Process the form data (e.g., save to database, send email, etc.)
+  $sql = "SELECT
+        pmt.*
+    FROM `fact_payment` pmt
+      LEFT JOIN `fact_appointment` appt
+        ON appt.appt_id = pmt.appt_id
+      LEFT JOIN `fact_patient_log` log
+        ON log.ptn_log_id = pmt.ptn_log_id
+    WHERE
+      (appt.patient_id = '$auth_user_id' OR log.patient_id = '$auth_user_id')
+    ORDER BY
+      `created_at` DESC,
+      `updated_at` DESC";
+  if ($sql) {
+    $result = $conn->query($sql);
+    if ($result) {
+      // Kiểm tra và hiển thị dữ liệu
+      $data = [];
+      while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+      }
+      echo json_encode(["status" => "success", "data" => $data]);
+    } else {
+      http_response_code(500);
+      echo json_encode(["status" => "error", "message" => "dbError"]);
+    }
+  }
+}
+?>
